68a10d5a02a962ff74516f1383c017fdf3f148d2
Khyber <kkysen@gmail.com>
2018-04-26 13:17:12 -0400

    added tests

--- sets.py
+++ sets.py
@@ -1,5 +1,12 @@
+# Khyber Sen and Michael Ruvinshteyn
+# SoftDev2 pd7
+# K16 -- Ready, Set, Math!
+# 2018-04-26
+
 from __future__ import print_function
 
+
+# See https://en.wikipedia.org/wiki/Set_(mathematics)#Basic_operations for basic set operations
 
 def difference(S, T):
     return [e for e in S if e not in T]
@@ -9,6 +16,7 @@ def union(S, T):
     return difference(S, T) + T
 
 
+#
 def intersection(S, T):
     return [e for e in union(S, T) if e in S and e in T]
 
@@ -22,7 +30,10 @@ def cartesian_product(S, T):
 
 
 def test():
+    print(union([-1, 0, 1, 2, 3], [2, 3, 4, 5]))
     print(intersection([1, 2, 3], [2, 3, 4]))
+    print(symmetric_difference([1, 2, 3], [2, 3, 4]))
+    print(cartesian_product([1, 2, 3], [2, 3, 4]))
 
 
 if __name__ == '__main__':
